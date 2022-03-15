8e94cd95edc5a45e03826b9212b6db3f3ceebda8
Fernando <fernando.hidalgo@desoft.cu>
2022-03-15 15:18:47 -0400

    SwapiTechnicalTest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,13 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
 import { ContainerComponent } from './layout/container/container.component';
 
 import { DataModule } from './data/data.module';
-import { ModulesModule } from './modules/modules.module';
+
+import { FilmsModule } from './modules/films/films.module';
+import { PeopleModule } from './modules/people/people.module';
+import { PlanetsModule } from './modules/planets/planets.module';
+import { SpeciesModule } from './modules/species/species.module';
+import { StarshipsModule } from './modules/starships/starships.module';
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
 
 
 @NgModule({
@@ -24,7 +30,12 @@ import { ModulesModule } from './modules/modules.module';
     BrowserModule,
     AppRoutingModule,
     DataModule,
-    ModulesModule,
+    FilmsModule,
+    PeopleModule,
+    PlanetsModule,
+    SpeciesModule,
+    StarshipsModule,
+    VehiclesModule,
     FormsModule
   ],
   providers: [],
